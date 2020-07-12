eb677d956ebefce99fd068fc75392feadeab2390
jackwangmeng <3290252143@qq.com>
2020-07-13 07:40:04 +0800

    no message

---
+++ src/com/wm/LeeCode/array/LeeCode1343.java
@@ -0,0 +1,24 @@
+package com.wm.LeeCode.array;
+
+public class LeeCode1343 {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int sum = 0 , result = 0;
+        int sumTarget = k * threshold;
+        for (int i = 0; i < k; i++){
+            sun += arr[i];
+        }
+        int addr = sum - sumTarget;
+        if (addr >= 0){
+            result++;
+        }
+        int pos = k;
+        for (int i = 0; i < arr.length - k; i++){
+            addr = addr + arr[pos] - arr[i];
+            if (addr >= 0){
+                result++;
+            }
+            pos++;
+        }
+        return result;
+    }
+}
