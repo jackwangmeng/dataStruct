0dbde5c91f4903adbf7ade97df7dafb2a0d18d2d
wm <3290252143@qq.com>
2020-10-13 08:51:40 +0800

    做31题

---
+++ src/com/wm/LeeCode/LeeCode/LeeCode31.java
@@ -0,0 +1,12 @@
+package com.wm.LeeCode.LeeCode;
+
+/**
+ * @author wm
+ * @Package com.wm.LeeCode.LeeCode
+ * @date 2020/10/13 8:50
+ */
+public class LeeCode31 {
+    public void nextPermutation(int[] nums) {
+
+    }
+}
