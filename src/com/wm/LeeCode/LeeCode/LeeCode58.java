995b5a06fbf035bb47ebe985898d70e9df87356a
wm <3290252143@qq.com>
2020-11-03 18:08:17 +0800

    简单难度

---
+++ src/com/wm/LeeCode/LeeCode/LeeCode58.java
@@ -0,0 +1,41 @@
+package com.wm.LeeCode.LeeCode;
+
+/**
+ * @author wm
+ * @Package com.wm.LeeCode.LeeCode
+ * @date 2020/11/3 17:49
+ */
+public class LeeCode58 {
+    public int lengthOfLastWord(String s) {
+        /*if (!s.trim().isEmpty()){
+            if (s.contains(" ")){
+                String[] str = s.split(" ");
+                int n = str[str.length-1].length();
+                return n;
+            }
+            return s.length();
+        }
+
+        return 0;*/
+        /*s = s.trim();
+        int start = s.lastIndexOf(" ") - 1;
+        return s.substring(start).length();*/
+        if (s == null || s.length() == 0)
+            return 0;
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--){
+            if (s.charAt(i) == ' '){
+                if (count == 0)
+                    continue;
+
+            }
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String str = " ";
+        System.out.println(new LeeCode58().lengthOfLastWord(str));
+    }
+}
