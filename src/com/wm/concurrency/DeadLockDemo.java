93b32f4d885a62197b5eec4a3353fcc9d1f612a3
jackwangmeng <3290252143@qq.com>
2020-11-23 00:07:55 +0800

    current

---
+++ src/com/wm/concurrency/DeadLockDemo.java
@@ -0,0 +1,43 @@
+package com.wm.concurrency;
+import com.wm.IO.T;
+import com.wm.desiger.api.A;
+
+public class DeadLockDemo {
+
+    private static String a = "A";
+    private static String b = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (a){
+                    try {
+                        Thread.currentThread().sleep(2000);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (b){
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (b){
+                    System.out.println("2");
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
