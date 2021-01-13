5999c1574867d3ba4e707ba0075d39328081a78b
wm <3290252143@qq.com>
2021-01-13 18:01:14 +0800

    多线程

---
+++ src/com/wm/thread/synch/Bank.java
@@ -0,0 +1,56 @@
+package com.wm.thread.synch;
+
+import com.wm.thread.twoRealizeOfThread.ThreadTest;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wm
+ * @Package com.wm.thread.synch
+ * @date 2021/1/13 14:17
+ */
+public class Bank {
+    private final double[] accounts;
+    private Lock bankLock;
+    private Condition sufficientFunds;
+
+    public Bank(int n, double initialBalance) {
+        accounts = new double[n];
+        Arrays.fill(accounts, initialBalance);
+        bankLock = new ReentrantLock();
+        sufficientFunds = bankLock.newCondition();
+    }
+
+    public void transfer(int from, int to, double amount) throws InterruptedException {
+        bankLock.lock();
+        try {
+            while (accounts[from] < amount) {
+                sufficientFunds.await();
+            }
+            System.out.println(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf("%10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf("Total Balance: %10.2f%n", getTotalBalance());
+            sufficientFunds.signalAll();
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    public double getTotalBalance() {
+        bankLock.lock();
+        try{
+            double sum = 0;
+            for (double a : accounts) {
+                sum += a;
+            }
+            return sum;
+        }finally {
+            bankLock.unlock();
+        }
+    }
+}
