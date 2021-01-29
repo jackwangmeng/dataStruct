8631f9d049f20e62be56baf39ba92e8b4bf101a5
wm <3290252143@qq.com>
2021-01-29 16:57:54 +0800

    no message

---
+++ mysql-connector/src/main/java/com/wm/mysqlconnector/test/Aobing.java
@@ -0,0 +1,10 @@
+package com.wm.mysqlconnector.test;
+
+/**
+ * @author wm
+ * @Package com.wm.mysqlconnector.test
+ * @date 2021/1/29 15:13
+ */
+public interface Aobing {
+    void say();
+}
---
+++ mysql-connector/src/main/java/com/wm/mysqlconnector/test/ShuaiAobing.java
@@ -0,0 +1,13 @@
+package com.wm.mysqlconnector.test;
+
+/**
+ * @author wm
+ * @Package com.wm.mysqlconnector.test
+ * @date 2021/1/29 15:14
+ */
+public class ShuaiAobing implements Aobing {
+    @Override
+    public void say() {
+        System.out.println("Aobing very cool");
+    }
+}
