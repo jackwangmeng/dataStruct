1baac43a7ec9f096c3ecf87a8c86ffeb46ce4657
wm <3290252143@qq.com>
2021-01-29 18:04:33 +0800

    no message

--- interview/src/main/java/com/wm/interview/leeCode/Test_spiralOrder.java
+++ interview/src/main/java/com/wm/interview/leeCode/Test_spiralOrder.java
@@ -1,9 +1,42 @@
 package com.wm.interview.leeCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wm
  * @Package com.wm.interview.leeCode
  * @date 2021/1/29 17:06
  */
 public class Test_spiralOrder {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                result.add(matrix[top][column]);
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                result.add(matrix[row][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    result.add(matrix[bottom][column]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    result.add(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return result;
+    }
 }
