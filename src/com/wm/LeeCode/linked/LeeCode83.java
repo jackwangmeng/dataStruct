4ddf12f4dc8699786b4b7decf7827b50ffb01149
jackwangmeng <3290252143@qq.com>
2021-02-14 11:33:25 +0800

    83删除重复元素

---
+++ src/com/wm/LeeCode/linked/LeeCode83.java
@@ -0,0 +1,16 @@
+package com.wm.LeeCode.linked;
+
+public class LeeCode83 {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
