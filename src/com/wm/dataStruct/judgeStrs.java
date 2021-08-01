599b34ebc2db708d83e3a8487bc1ab7745434bf6
“jackwangmeng” <3290252143@qq.com>
2021-08-01 22:24:57 +0800

    no message

--- src/com/wm/dataStruct/judgeStrs.java
+++ src/com/wm/dataStruct/judgeStrs.java
@@ -1,2 +1,15 @@
-package com.wm.dataStruct;public class judgeStrs {
+package com.wm.dataStruct;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class judgeStrs {
+    public static void main(String[] args) {
+        String s = Stream.of("123", "", "12312")
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse("12");
+
+        System.out.println(s);
+    }
 }
